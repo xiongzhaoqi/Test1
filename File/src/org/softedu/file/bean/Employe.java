4450eac0c3e1a4ee4f18de60fb42a8538b899f5c
Administrator <Administrator@PC-20160118NAPA>
2017-01-05 21:53:56 +0800

    出事1

---
+++ File/src/org/softedu/file/bean/Employe.java
@@ -0,0 +1,61 @@
+package org.softedu.file.bean;
+
+public class Employe {
+private int eid;
+private String name;
+private String sex;
+private String department;
+private String position;
+private String employedtime;
+private String telphone;
+private String password;
+public int getEid() {
+	return eid;
+}
+public void setEid(int eid) {
+	this.eid = eid;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSex() {
+	return sex;
+}
+public void setSex(String sex) {
+	this.sex = sex;
+}
+public String getDepartment() {
+	return department;
+}
+public void setDepartment(String department) {
+	this.department = department;
+}
+public String getPosition() {
+	return position;
+}
+public void setPosition(String position) {
+	this.position = position;
+}
+public String getEmployedtime() {
+	return employedtime;
+}
+public void setEmployedtime(String employedtime) {
+	this.employedtime = employedtime;
+}
+public String getTelphone() {
+	return telphone;
+}
+public void setTelphone(String telphone) {
+	this.telphone = telphone;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
