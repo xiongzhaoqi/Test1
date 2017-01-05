bfa03618a1c82df92cd837d7bc49dd957ff358a2
Administrator <Administrator@PC-20160118NAPA>
2017-01-05 21:53:06 +0800

    初始化项目

---
+++ File/src/org/softedu/file/bean/Depart.java
@@ -0,0 +1,33 @@
+package org.softedu.file.bean;
+
+public class Depart {
+private int id;
+private String department;
+private String manager;
+private String info;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getDepartment() {
+	return department;
+}
+public void setDepartment(String department) {
+	this.department = department;
+}
+public String getManager() {
+	return manager;
+}
+public void setManager(String manager) {
+	this.manager = manager;
+}
+public String getInfo() {
+	return info;
+}
+public void setInfo(String info) {
+	this.info = info;
+}
+
+}
